28413f465918e07b4174912f8899c64d3a53dd69
DylanVause <herecomesdylan@yahoo.com>
2023-12-13 15:37:18 -0800

    Update functions.js

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -1,12 +1,5 @@
 
 
-/**
- * Adds two numbers
- * @CustomFunction
- * @param {number} first The first number
- * @param {number} second The second number
- * @returns The sum of the two numbers
- */
 function custom_add(first, second) {
     return first + second;
 }
