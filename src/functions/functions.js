fe4e61a5192aa7edd9fff3053a5e60c45bd11f76
DylanVause <Dylan.Vause@westfraser.com>
2023-12-14 09:40:07 -0800

    Update functions.js

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -6,9 +6,37 @@
  * @param {number} hour 
  * @returns
  */
-function AesoPoolPrice(date, hour) {
+async function AesoPoolPrice(date, hour) {
     console.log(localStorage.getItem('aeso-api-key'));
-    return -1;
+
+    const js_date = dateFromSerial(date);
+    let year = js_date.getFullYear().toFixed();
+    let month = js_date.getMonth().toFixed();
+    let day = js_date.getDate().toFixed();
+    month = month.padStart(2-month.length, "0");
+    day = month.padStart(2-day.length, "0");
+    const endpoint = `https://api.aeso.ca/v1.1/price/poolPrice?startDate=${year}-${month}-${day}&endDate=${year}-${month}-${day}`;
+
+    const response = await fetch(endpoint, {
+        headers: {
+            'X-API-Key': localStorage.getItem('aeso-api-key')   
+        }
+    });
+
+    const json = await response.json();
+
+    return JSON.stringify(json, null, 4);
 }
 
 CustomFunctions.associate("AesoPoolPrice", AesoPoolPrice);
+
+/**Converts an Excel date serial into a Date object.
+ * 
+ * @param {number} serial - The Excel date serial number
+ * @returns 
+ */
+function dateFromSerial(serial) {
+    const utc_days = Math.floor(serial - 25569);
+    const utc_value = utc_days * 86400;
+    return new Date(utc_value * 1000);
+}
