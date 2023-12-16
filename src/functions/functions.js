6ec6399f69c5f1972cbd23231d265a2f389224ef
DylanVause <Dylan.Vause@westfraser.com>
2023-12-15 16:23:28 -0800

    update

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -11,8 +11,8 @@ async function AesoPoolPrice(date, hour) {
 
     const js_date = dateFromSerial(date);
     let year = js_date.getUTCFullYear().toFixed();
-    let month = js_date.getUTCMonth().toFixed();
-    let day = js_date.getUTCDate().toFixed();
+    let month = (js_date.getUTCMonth() + 1).toFixed();
+    let day = (js_date.getUTCDate() + 1).toFixed();
     month = month.padStart(2, "0");
     day = month.padStart(2, "0");
     const endpoint = `http://localhost:38820/https://api.aeso.ca/report/v1.1/price/poolPrice?startDate=${year}-${month}-${day}&endDate=${year}-${month}-${day}`;
@@ -43,20 +43,20 @@ CustomFunctions.associate("AesoPoolPrice", AesoPoolPrice);
  * @returns 
  */
 function dateFromSerial(xlSerial) {
-  // each serial up to 60 corresponds to a valid calendar date.
-  // serial 60 is 1900-02-29. This date does not exist on the calendar.
-  // we choose to interpret serial 60 (as well as 61) both as 1900-03-01
-  // so, if the serial is 61 or over, we have to subtract 1.
-  if (xlSerial < 61) {
-    xlSerial = xlSerial;
-  }
-  else {
-    xlSerial = xlSerial - 1;
-  }
+    // each serial up to 60 corresponds to a valid calendar date.
+    // serial 60 is 1900-02-29. This date does not exist on the calendar.
+    // we choose to interpret serial 60 (as well as 61) both as 1900-03-01
+    // so, if the serial is 61 or over, we have to subtract 1.
+    if (xlSerial < 61) {
+        xlSerial = xlSerial;
+    }
+    else {
+        xlSerial = xlSerial - 1;
+    }
 
-  let timestamp = (xlSerial-25568) * 24 * 3600*1000;
+    let timestamp = (xlSerial - 25568) * 24 * 3600 * 1000;
 
-  return new Date(timestamp);
+    return new Date(timestamp);
 }
 
 /**Converts an Excel date to a JavaScript date.
