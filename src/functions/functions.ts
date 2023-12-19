7db5f54f406293b4a6a84882ce09934839c9ccfb
DylanVause <136635591+Dylan-Vause@users.noreply.github.com>
2023-12-18 16:47:01 -0800

    Error Logging

--- src/functions/functions.ts
+++ src/functions/functions.ts
@@ -10,21 +10,26 @@ import { parseDate } from "./xlDate";
  * @returns {Promise<number>}
  */
 async function AesoPoolPrice(date: number | string, hour: number): Promise<number> {
-    let js_date = parseDate(date);
-    const json = await pushRequest(js_date);
+    try {
+        let js_date = parseDate(date);
+        const json = await pushRequest(js_date);
 
-    const begin_datetime_mpt = `${js_date.getUTCFullYear()
-        }-${(js_date.getUTCMonth() + 1).toString().padStart(2, '0')
-        }-${js_date.getUTCDate().toString().padStart(2, '0')
-        } ${(hour - 1).toString().padStart(2, '0')
-        }:00`
-    for (const item of json.return["Pool Price Report"]) {
-        if (item.begin_datetime_mpt == begin_datetime_mpt) {
-            return parseFloat(item.pool_price);
+        const begin_datetime_mpt = `${js_date.getUTCFullYear()
+            }-${(js_date.getUTCMonth() + 1).toString().padStart(2, '0')
+            }-${js_date.getUTCDate().toString().padStart(2, '0')
+            } ${(hour - 1).toString().padStart(2, '0')
+            }:00`
+        for (const item of json.return["Pool Price Report"]) {
+            if (item.begin_datetime_mpt == begin_datetime_mpt) {
+                return parseFloat(item.pool_price);
+            }
         }
-    }
 
-    throw new Error("Not found.")
+        throw new Error("Not found.")
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
 
     // return json;
 }
@@ -37,9 +42,14 @@ async function AesoPoolPrice(date: number | string, hour: number): Promise<numbe
  * @returns {Promise<number>}
  */
 async function AesoMeteredVolume(asset_id: string, date: number | string, hour: number): Promise<number> {
-    let js_date = parseDate(date);
-    const volume = await getMeteredVolume(asset_id, new Date(js_date.getTime() + (hour - 1) * 3600_000));
-    return volume;
+    try {
+        let js_date = parseDate(date);
+        const volume = await getMeteredVolume(asset_id, new Date(js_date.getTime() + (hour - 1) * 3600_000));
+        return volume;
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
 }
 
 /**
